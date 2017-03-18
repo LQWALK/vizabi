9e22264b83ac944598f0dab96a28b34cc8be62c1
aminaly <ly.ami@husky.neu.edu>
2017-03-18 16:55:25 -0400

    sankey_componenet outlining

--- src/tools/sankeydiagram/sankey-component.js
+++ src/tools/sankeydiagram/sankey-component.js
@@ -11,56 +11,85 @@ const SankeyComponent = Component.extend({
 	 * @param {Object} config The options passed to the component
 	 * @param {Object} context The component's parent
 	 */
-  init(config, context) {
-    const _this = this;
-    this.name = "sankey-component";
-    this.template = require("./sankeydiagram.html");
+	init(config, context) {
+		const _this = this;
+		this.name = "sankey-component";
+		this.template = require("./sankeydiagram.html");
 
 		// expectectations for model
-    this.model_expects = [{
-      name: "time",
-      type: "time"
-    }, {
-      name: "entities",
-      type: "entities"
-    }, {
-      name: "marker",
-      type: "model"
-    }, {
-      name: "locale",
-      type: "locale"
-    }, {
-      nam: "ui",
-      type: "ui"
-    }];
+		this.model_expects = [{
+			name: "time",
+			type: "time"
+		}, {
+			name: "entities",
+			type: "entities"
+		}, {
+			name: "marker",
+			type: "model"
+		}, {
+			name: "locale",
+			type: "locale"
+		}, {
+			nam: "ui",
+			type: "ui"
+		}];
 
-    this.model_binds = {
-      "change:time.value": function() {
-        if (!_this._readyOnce) return;
-      },
-      "change:marker": function(evt, path) {
-        if (!_this._readyOnce) return;
-      },
-      "change:marker.highlight": function() {
-        if (!_this._readyOnce) return;
-      },
-      "change:marker.select": function() {
-        if (!_this._readyOnce) return;
-      },
-      "change:marker.opacitySelectDim": function() {
-        if (!_this._readyOnce) return;
-      },
-      "change:marker.opacityRegular": function() {
-        if (!_this._readyOnce) return;
-      }
-    };
+		this.model_binds = {
+			"change:time.value": function() {
+				if(!_this._readyOnce) return;
+			},
+			"change:marker": function(evt, path) {
+				if(!_this._readyOnce) return;
+			},
+			"change:marker.highlight": function() {
+				if(!_this._readyOnce) return;
+			},
+			"change:marker.select": function() {
+				if(!_this._readyOnce) return;
+			},
+			"change:marker.opacitySelectDim": function() {
+				if(!_this._readyOnce) return;
+			},
+			"change:marker.opacityRegular": function() {
+				if(!_this._readyOnce) return;
+			}
+		};
 
-    this._super(config, context);
+		this._super(config, context);
 
-    this.isDataPreprocessed = false;
+		this.isDataPreprocessed = false;
 
 
-  }
+	},
+
+	readyOnce() {
+
+		this.element = d3.select(this.element);
+
+		// select elements here before chart
+
+		const _this = this;
+
+		// set up resizing
+		this.on("resize", () => {
+			//return if updatesize exists with error
+			if(_this.updateSize()) return;
+			_this.updateMarkerSizeLimits();
+			_this._labels.updateSize();
+			_this.redrawDataPoints();
+			//_this._selectlist.redraw();
+
+		});
+
+		this.initChart();
+
+
+	},
+
+	initChart() {
+
+	}
+
 });
 
 export default SankeyComponent;
